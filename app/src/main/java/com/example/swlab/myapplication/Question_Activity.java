c757170ef837a66a9eac291fa2dfbea4142faf4b
Swlabgroup2 <swlabgroup2@gmail.com>
2017-05-13 15:39:25 +0800

    2017/05/13 葉

--- app/src/main/java/com/example/swlab/myapplication/Question_Activity.java
+++ app/src/main/java/com/example/swlab/myapplication/Question_Activity.java
@@ -3,31 +3,14 @@ package com.example.swlab.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.Query;
-import com.firebase.client.ValueEventListener;
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class Question_Activity extends AppCompatActivity {
     private ArrayList<DB_Question> list=new ArrayList<>();
