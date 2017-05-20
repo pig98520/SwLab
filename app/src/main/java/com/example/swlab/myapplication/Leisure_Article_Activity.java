8d96bf69944e1a69e503b6dff545b92e97411911
Swlabgroup2 <swlabgroup2@gmail.com>
2017-05-20 21:33:20 +0800

    2017/05/20 葉

--- app/src/main/java/com/example/swlab/myapplication/Leisure_Article_Activity.java
+++ app/src/main/java/com/example/swlab/myapplication/Leisure_Article_Activity.java
@@ -1,6 +1,11 @@
 package com.example.swlab.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -10,8 +15,21 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 public class Leisure_Article_Activity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
     public static final String API_KEY="AIzaSyCSoju_ikeadgsz-TL4ipk5AI1bQSw0wSQ";
-    public static final String VIDEO_ID="_kOyO5Jg8wk";
+    //public static final String VIDEO_ID="_kOyO5Jg8wk";
+    private String videoArray[]= new String []
+            {"NLElzEJPceA",
+            "uiJ4zibW8_M",
+            "ITxWUu6UcWQ&t=3s",
+            "zxeR614s1mE",
+            "_RTsJGwt_5c",
+            "tmYa6ubl4Ew",
+            "Ns6xmlkjtbg",
+            "vFWpKOlpLnQ"};
     private YouTubePlayerView youTubePlayerView;
+    private Spinner spinner;
+    private Button confirm;
+    private ArrayAdapter<String> adapter;
+    private int video_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,20 +40,43 @@ public class Leisure_Article_Activity extends YouTubeBaseActivity implements You
 
     private void processView() {
         youTubePlayerView=(YouTubePlayerView)findViewById(R.id.youtube_player);
-        youTubePlayerView.initialize(API_KEY,this);
+        youTubePlayerView.initialize(API_KEY,Leisure_Article_Activity.this);
+        spinner=(Spinner)findViewById(R.id.spinner);
+        confirm=(Button)findViewById(R.id.btn_confirm);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, videoArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(selectListener);
     }
 
     private void processControl() {
-
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 
+    private AdapterView.OnItemSelectedListener selectListener=new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            video_id=spinner.getSelectedItemPosition();
+            Toast.makeText(Leisure_Article_Activity.this,video_id+"",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
         youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
         youTubePlayer.setPlaybackEventListener(playbackEventListener);
         if(!b)
-            youTubePlayer.cueVideo(VIDEO_ID);
+           youTubePlayer.cueVideo(videoArray[video_id]);
     }
+
 
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
