42a599bc48a86da9937494e9e3ab45c7ffb390b4
pig98520 <pig98520@hotmail.com>
2017-06-02 16:37:36 +0800

    2017/06/02 葉

--- app/src/main/java/com/example/swlab/myapplication/Sports_Run.java
+++ app/src/main/java/com/example/swlab/myapplication/Sports_Run.java
@@ -1,24 +1,38 @@
 package com.example.swlab.myapplication;
 
+import android.content.DialogInterface;
+import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.firebase.client.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Sports_Run extends AppCompatActivity {
-    private DatePicker date;
-    private EditText cal;
-    private EditText distance;
-    private EditText time;
-    private Button submit;
-    private String sportDate;
-    private String Cal;
-    private String Distance;
+    private EditText edt_cal;
+    private EditText edt_distance;
+    private EditText edt_time;
+    private SimpleDateFormat dtFormat;
+    private String nowTime;
+    private Date date;
+    private TextView timer;
+    private Button save;
+    private Button start;
+    private int sec =0;
+    private int min=0;
+    private CountDownTimer countdownTimer;
+    private Boolean isTimer=false;
+    private String cal;
+    private String distance;
     private String sportTime;
     private FirebaseAuth auth;
 
@@ -30,28 +44,128 @@ public class Sports_Run extends AppCompatActivity {
         processControl();
     }
 
+
     private void processView() {
-        date=(DatePicker)findViewById(R.id.datePicker);
-        cal=(EditText)findViewById(R.id.txtCal);
-        distance=(EditText)findViewById(R.id.txtDistance);
-        time=(EditText)findViewById(R.id.txtTime);
-        submit=(Button)findViewById(R.id.submit);
-        auth= FirebaseAuth.getInstance();
+        edt_cal = (EditText) findViewById(R.id.txtCal);
+        edt_distance = (EditText) findViewById(R.id.txtDistance);
+        edt_time = (EditText) findViewById(R.id.txtTime);
+        timer=(TextView)findViewById(R.id.txt_timer);
+        save = (Button) findViewById(R.id.btn_save);
+        start = (Button) findViewById(R.id.btn_start);
+        auth = FirebaseAuth.getInstance();
+        timer = (TextView)findViewById(R.id.txt_timer);
+        dtFormat = new SimpleDateFormat("yyyy/MM/dd");
+        date = new Date();
+        nowTime = dtFormat.format(date);
     }
 
     private void processControl() {
-        submit.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                sportDate=(date.getYear()+"-"+(date.getMonth()+1)+"-"+date.getDayOfMonth());
-                Cal=cal.getText().toString()+" 大卡";
-                Distance=distance.getText().toString()+" 公尺";
-                sportTime=time.getText().toString()+" 分鐘";
-                insertDate(sportDate,Cal,Distance,sportTime);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isTimer){
+                    isTimer=true;
+                    timerStart();
+                }
+                else
+                {
+                    isTimer=false;
+                    timerStop();
+                }
+
+            }
+        });
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cal=edt_cal.getText().toString().trim();
+                distance=edt_distance.getText().toString().trim();
+                sportTime=edt_time.getText().toString().trim();
+                insertData(nowTime,cal,distance,sportTime);
+            }
+        });
+        timer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isTimer) {
+                    countdownTimer.cancel();
+                    isTimer=false;
+                }
+                else {
+                    countdownTimer.start();
+                    isTimer=true;
+                }
             }
         });
     }
 
-    private void insertDate(String sportDate,String Cal, String Distance, String sportTime){
+
+    private void timerStart() {
+        countdownTimer=new CountDownTimer(1000000000000L, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                sec++;
+                if(sec==60) {
+                    min++;
+                    sec=0;
+                }
+                if(min<10&&sec>10)
+                    timer.setText("0"+min+":"+sec);
+                else if(min>10&&sec<10)
+                    timer.setText(min+":0"+sec);
+                else if(min<10&sec<10)
+                    timer.setText("0"+min+":0"+sec);
+                else
+                    timer.setText(min+":"+sec);
+                start.setText("結束運動");
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+        countdownTimer.start();
+    }
+
+    private void timerStop() {
+        edt_time.setText(timer.getText().toString().trim());
+        edt_cal.setText(((min*60+sec)*0.1819)+"");
+        timer.setText("00:00");
+        min=0;
+        sec=0;
+        countdownTimer.cancel();
+        finishDialog();
+    }
+
+    private void finishDialog() {
+        AlertDialog.Builder finishDialog=new AlertDialog.Builder(this);
+        final EditText input=new EditText(this);
+        finishDialog.setTitle("結束運動");
+        finishDialog.setMessage("流點汗應該舒服多了吧,請輸入今天運動的距離以儲存紀錄");
+        finishDialog.setView(input);
+        DialogInterface.OnClickListener confirmClick =new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                edt_distance.setText(input.getText().toString());
+                cal=edt_cal.getText().toString().trim();
+                distance=edt_distance.getText().toString().trim();
+                sportTime=edt_time.getText().toString().trim();
+                insertData(nowTime,cal,distance,sportTime);
+            }
+        };
+        DialogInterface.OnClickListener cancelClick =new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        };
+        finishDialog.setNeutralButton("確定",confirmClick);
+        finishDialog.setNegativeButton("取消",cancelClick);
+        finishDialog.show();
+    }
+
+    private void insertData(String sportDate, String Cal, String Distance, String sportTime){
         Firebase myFirebaseRef = new Firebase("https://swlabapp.firebaseio.com/user");
         Firebase userRef = myFirebaseRef.child("sport").child("run").child(auth.getCurrentUser().getUid().trim());
         DB_Sports_Bike_Run_Swim data = new DB_Sports_Bike_Run_Swim(sportDate,Cal,Distance,sportTime);
