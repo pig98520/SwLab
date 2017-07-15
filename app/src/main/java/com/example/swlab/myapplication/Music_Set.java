69ba04e0c52903029825e5502a6e13f509c628e8
Swlabgroup2 <swlabgroup2@gmail.com>
2017-07-15 13:34:55 +0800

    2017/07/15 葉 music設定頁面儲存至firebase

--- app/src/main/java/com/example/swlab/myapplication/Music_Set.java
+++ app/src/main/java/com/example/swlab/myapplication/Music_Set.java
@@ -1,21 +1,48 @@
 package com.example.swlab.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
+import android.widget.RadioButton;
+
+import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Music_Set extends AppCompatActivity {
-
+    private Button setBtn;
+    private Button saveBtn;
+    private EditText hour;
+    private EditText min;
+    private RadioButton random;
+    private RadioButton normal;
+    private int time;
+    private String mode;
+    private FirebaseAuth auth;
+    private Firebase firebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.music_set);
-        Button setBtn = (Button)findViewById(R.id.back_btn);
+        processView();
+        processContral();
+        hour.requestFocus();
+    }
+
+    private void processView() {
+        setBtn = (Button)findViewById(R.id.back_btn);
+        saveBtn=(Button)findViewById(R.id.save_btn);
+        hour= (EditText) findViewById(R.id.hr_edit);
+        min=(EditText)findViewById(R.id.min_edit);
+        random=(RadioButton)findViewById(R.id.ram_radio);
+        normal=(RadioButton)findViewById(R.id.normal_radio);
+        auth= FirebaseAuth.getInstance();
+    }
+
+    private void processContral() {
         setBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,14 +71,20 @@ public class Music_Set extends AppCompatActivity {
                 }
             }
         });
-        RadioGroup mode=(RadioGroup) findViewById(R.id.radGroup) ;
-        if (mode.getCheckedRadioButtonId()==R.id.normal_radio){
 
-        }
-        else{
-
-        }
-        EditText hour= (EditText) this.findViewById(R.id.hr_edit);
-        hour.requestFocus();
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                time=Integer.parseInt(hour.getText().toString().trim())*60
+                    +Integer.parseInt(min.getText().toString().trim());
+                if(random.isChecked())
+                    mode="random";
+                else
+                    mode="normal";
+                firebaseRef=new Firebase("https://swlabapp.firebaseio.com/user/musicsetting/"+auth.getCurrentUser().getUid());
+                firebaseRef.child("mode").setValue(mode);
+                firebaseRef.child("time").setValue(time);
+            }
+        });
     }
 }
